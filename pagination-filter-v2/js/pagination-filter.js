d04d915b777368478caca07dc01b836a160eb994
Rod-S <33836692+Rod-S@users.noreply.github.com>
2018-01-30 17:41:02 -0600

    -created defaultLoad function for initial pageload function calls

--- pagination-filter-v2/js/pagination-filter.js
+++ pagination-filter-v2/js/pagination-filter.js
@@ -1,3 +1,22 @@
+//custom jQuery selector ':Contains' to remove DOM case-sensitivity
+$(function ($) {
+  $.expr[':'].Contains = function(object, index, meta) {
+    return (object.innerText).toUpperCase()
+    .indexOf( meta[3].toUpperCase() )>=0;
+  }
+});
+
+const defaultLoad = () => {
+  //call showPage(); page 1 displays by default--loads on website load
+  showPage(1,$('li.student-item'));
+  //call appendPageLinks
+  appendPageLinks($('li.student-item'));
+  //active class called on page load's default link 1
+  $('a').get(0).className = 'active';
+  //call searchList()
+  searchList($('li.student-item'));
+};
+
 //display correct list of 10 students based on page number
 const showPage = (pageNumber,studentList) => {
   //hide full list of students given by inline HTML markup
@@ -6,7 +25,7 @@ const showPage = (pageNumber,studentList) => {
   studentList.slice(((pageNumber-1)*10),((pageNumber*10))).show();
 };
 
-//allow user to select which page of students to display based on link clicked
+//allow user to select which page of students to display based upon link clicked
 const appendPageLinks = (studentList) => {
   //determine how many total pages (of 10 students each) there are for this student list
   const totalPages = Math.ceil(studentList.length/10);
@@ -34,18 +53,12 @@ const appendPageLinks = (studentList) => {
   });
 };
 
-//custom jQuery selector ':Contains' to remove DOM case-sensitivity of searchList()'s .find()
-$(function ($) {
-  $.expr[':'].Contains = function(object, index, meta) {
-    return (object.innerText).toUpperCase()
-    .indexOf( meta[3].toUpperCase() )>=0;
-  }
-});
-
-//creation of search bar and student search functionality
+//creation of input box, button and student search functionality
 function searchList(studentList) {
   $('div.page-header').append('<div class="student-search"><input id=search placeholder="Search for students..."><button id=searchButton>Search</button>');
   $('#searchButton').on("click", function() {
+    //removal of DOM message to avoid duplication during multiple failed search attempts
+    $('#noStudent').remove();
     //remove class to avoid previously matched students from appearing
     $('li').removeClass('match');
     let searchFilter = $(this).prev().val().toUpperCase();
@@ -53,7 +66,6 @@ function searchList(studentList) {
     //then hides or shows names based on checks
     if (searchFilter != '') {
               //removal of DOM message to avoid duplication during multiple failed search attempts
-      $('#noStudent').remove();
       //show or hide elements that are matched to searchFilter input
       $(studentList).find("h3:Contains(" + searchFilter + ")").parent().parent().show();
       $(studentList).find(".email:Contains(" + searchFilter + ")").parent().parent().show();
@@ -74,30 +86,25 @@ function searchList(studentList) {
       };
       //conditional for message to append to DOM if no students found
       if ($('.student-item').is(':visible') == false) {
-        //removal of DOM message to avoid duplication during multiple failed search attempts
-        $('#noStudent').remove();
         //remove class to avoid previously matched students from appearing
         $('li').removeClass('match');
+        //conditional for alert to run if input box is blank
         $('.student-list').prepend('<div id=noStudent><h2>No Students Found with Name or Email:  ' + $(this).prev().val() + '  </h2></div>');
       }
-      //conditional for alert to run if input box is blank
     } else if (searchFilter == '') {
-      $('#noStudent').remove();
       //remove class to avoid previously matched students from appearing
       $('li').removeClass('match');
-      //Call showPage and appendPageLinks to revert to original full student list
-      showPage(1,$('li.student-item'));
-      appendPageLinks($('li.student-item'));
+      //Remove pagination div to avoid pageLink duplication between $match and original studentList
+      $('.pagination').remove();
+      //Remove previous div containing input/button to avoid duplication
+      $('.student-search').remove();
+      //call defaultLoad function
+      defaultLoad();
+      //empty search field message
       alert('Please enter a name or email address of the student you would like to find.');
     }
   });
 };
 
-//call showPage(); page 1 displays by default--loads on website load
-showPage(1,$('li.student-item'));
-//call appendPageLinks
-appendPageLinks($('li.student-item'));
-//active class called on page load's default link 1
-$('a').get(0).className = 'active';
-//call searchList()
-searchList($('li.student-item'));
+//load webpage
+defaultLoad();
